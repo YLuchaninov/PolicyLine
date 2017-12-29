2afcb2dfc4f70e8e35655ff6910b6d3d69109779
Yuriy Luchaninov <yuriy.luchaninov@gmail.com>
2017-12-29 15:59:44 +0200

    'chain' & 'compouse' policies tests

--- test/operations.js
+++ test/operations.js
@@ -75,6 +75,93 @@ describe("Operations", function () {
 
         expect(totalPolicy.check(user, action, env)).to.equal(true);
     });
+
+    it(": 'chain'", function () {
+        let rulesA = {
+            target: [
+                "user.role='admin'"
+            ],
+            effect: "permit",
+            algorithm: "all"
+        };
+
+        let rulesB = {
+            target: [
+                "user.location = env.location"
+            ],
+            effect: "permit",
+            algorithm: "all"
+        };
+
+        let rulesC = {
+            target: [
+                "action.name='save'"
+            ],
+            effect: "permit",
+            algorithm: "all"
+        };
+
+        let policyA = new Policy(rulesA);
+        let policyB = new Policy(rulesB);
+        let policyC = new Policy(rulesC);
+
+        let totalPolicy = policyA.and(policyB).and(policyC);
+
+        let user = {
+            role: 'admin',
+            location: 'NY'
+        };
+        let env = {
+            location: 'NY'
+        };
+        let action = {
+            name: 'save'
+        };
+
+        expect(totalPolicy.check(user, action, env)).to.equal(true);
+    });
+
+    it(": 'compose'", function () {
+        let adminRules = {
+            target: [
+                "user.role = 'admin'"
+            ],
+            effect: "permit",
+            algorithm: "all"
+        };
+
+        let userRules = {
+            target: [
+                "user.role='user'"
+            ],
+            effect: "permit",
+            algorithm: "all"
+        };
+
+        let locationRules = {
+            target: [
+                "user.location=resource.location"
+            ],
+            effect: "permit",
+            algorithm: "all"
+        };
+
+        let adminPolicy = new Policy(adminRules);
+        let userPolicy = new Policy(userRules);
+        let locationPolicy = new Policy(locationRules);
+
+        let totalPolicy = adminPolicy.or(userPolicy.and(locationPolicy));
+
+        let user = {
+            role: 'user',
+            location: 'NY'
+        };
+        let resource = {
+            location: 'NY'
+        };
+
+        expect(totalPolicy.check(user, null, null, resource)).to.equal(true);
+    });
 });
 
 
