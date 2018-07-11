473c46efce7da9e924d982a889ec68f67952d54b
Yuriy Luchaninov <yuriy.luchaninov@gmail.com>
2018-07-11 22:03:01 +0300

    small udate of date adapters

--- src/adapter.js
+++ src/adapter.js
@@ -1,17 +1,22 @@
 // todo create DI possibility for custom adapters
 
+function extractDate(a) {
+    if (a instanceof Date) return a;
+    return new Date(a);
+}
+
 const adapters = {
     'or': (a, context) => (a), //todo change
 
     'radius': (a, context) => (a), //todo change
     'position': (a, context) => (a), //todo change
 
-    'minute': (a) => ((new Date(a)).getMinutes()),
-    'day': (a) => ((new Date(a)).getDate()), // according to the local time
-    'hour': (a) => ((new Date(a)).getHours()), // according to the local time
-    'weekday': (a) => ((new Date(a)).getDay()), // according to the local time
-    'month': (a) => ((new Date(a)).getMonth()), // according to the local time
-    'year': (a) => ((new Date(a)).getFullYear()), // according to the local time
+    'minute': (a) => (extractDate(a).getMinutes()),
+    'day': (a) => (extractDate(a).getDate()), // according to the local time
+    'hour': (a) => (extractDate(a).getHours()), // according to the local time
+    'weekday': (a) => (extractDate(a).getDay()), // according to the local time
+    'month': (a) => (extractDate(a).getMonth()), // according to the local time
+    'year': (a) => (extractDate(a).getFullYear()), // according to the local time
 
     'toInt': (a) => (parseInt(a, 10)),
     'toString': (a) => (a + ''),
