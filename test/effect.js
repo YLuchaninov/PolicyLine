e1dd1967a52d4dda7fcdad462655fcad114717cf
Yuriy Luchaninov <yuriy.luchaninov@gmail.com>
2018-07-11 21:09:39 +0300

    effects was done

---
+++ test/effect.js
@@ -0,0 +1,113 @@
+let expect = require('chai').expect;
+let ABAC = require('../dist/policyline.min');
+
+let Policy = ABAC.Policy;
+
+describe("Effect Checking", function () {
+    it(": 'permit'", function () {
+        let rules = {
+            target: [
+                'user.name=="Joe"'
+            ],
+            effect: 'permit'
+        };
+
+        let policy = new Policy(rules);
+        let data = {
+            user: {
+                name: 'Joe'
+            }
+        };
+
+        expect(policy.check(data)).to.equal(true);
+    });
+
+    it(": 'permit' - negative case", function () {
+        let rules = {
+            target: [
+                'user.name=="Joe"'
+            ],
+            effect: 'permit'
+        };
+
+        let policy = new Policy(rules);
+        let data = {
+            user: {
+                name: 'Sam'
+            }
+        };
+
+        expect(policy.check(data)).to.equal(false);
+    });
+
+    it(": 'permit by default'", function () {
+        let rules = {
+            target: [
+                'user.name=="Joe"'
+            ]
+        };
+
+        let policy = new Policy(rules);
+        let data = {
+            user: {
+                name: 'Joe'
+            }
+        };
+
+        expect(policy.check(data)).to.equal(true);
+    });
+
+    it(": 'permit by default' - negative case", function () {
+        let rules = {
+            target: [
+                'user.name=="Joe"'
+            ]
+        };
+
+        let policy = new Policy(rules);
+        let data = {
+            user: {
+                name: 'Sam'
+            }
+        };
+
+        expect(policy.check(data)).to.equal(false);
+    });
+
+    it(": 'deny'", function () {
+        let rules = {
+            target: [
+                'user.name=="Joe"'
+            ],
+            effect: 'deny'
+        };
+
+        let policy = new Policy(rules);
+        let data = {
+            user: {
+                name: 'Joe'
+            }
+        };
+
+        expect(policy.check(data)).to.equal(false);
+    });
+
+    it(": 'deny' - negative case", function () {
+        let rules = {
+            target: [
+                'user.name=="Joe"'
+            ],
+            effect: 'deny'
+        };
+
+        let policy = new Policy(rules);
+        let data = {
+            user: {
+                name: 'Sam'
+            }
+        };
+
+        expect(policy.check(data)).to.equal(true);
+    });
+
+});
