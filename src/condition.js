5dbf59a34a1833849d42ccdd2d74573977b07756
Yuriy <y.luchaninov@mobidev.biz>
2018-07-24 12:21:20 +0300

    update

--- src/condition.js
+++ src/condition.js
@@ -13,8 +13,42 @@ function prepareCondition(inputData, exp, context) {
     };
 }
 
+const OperatorMap = {
+    '=': '$eq',	//Matches values that are equal to a specified value.
+    '==': '$eq', //Matches values that are equal to a specified value.
+    '!=': '$ne', //Matches all values that are not equal to a specified value.
+    '>=': '$gte', //Matches values that are greater than or equal to a specified value.
+    '>': '$gt',	//Matches values that are greater than a specified value.
+    '<=': '$lte', //Matches values that are less than or equal to a specified value.
+    '<': '$lt',	//Matches values that are less than a specified value.
+    '~=': '$in', //Matches any of the values specified in an array.
+    '^=': '$nin', //Matches none of the values specified in an array.
+};
+const repeat =()=>{};
+const MutatorMap = {
+    'or': ()=>{},
+    'radius':()=>{},
+    'inArea':()=>{}
+};
+
 const Adapter = {
-    MongoJSONb: a => a,
+    MongoJSONb: rules => {
+        const result = {};
+        let resource;
+        for (let rule of rules) {
+            resource = rule.resource.replace('resource.', '');
+
+            if (result[resource] && Array.isArray(result[resource])) {
+
+            } else if (result[resource]) {
+
+            } else {
+
+
+            }
+        }
+        return rules; // todo change to result
+    },
 };
 
 export {
